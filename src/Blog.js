0dbeee318f5545a95c215a1a82f8f98e4512d938
Ravi D. Pansuriya <49712335+Ravipatel1999@users.noreply.github.com>
2022-06-20 22:13:52 +0530

    app and blog changes

--- src/Blog.js
+++ src/Blog.js
@@ -4,16 +4,20 @@ import React, { useEffect, useState } from 'react'
 function Blog() {
 
   const [data, setData] = useState([])
-
-
+  const [flag, setFlag] = useState(false);
+  const [Index, setIndex] = useState(0);
   useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = () => {
     fetch('https://my-json-server.typicode.com/Codeinwp/front-end-internship-api/posts')
       .then((res) => {
         return res.json()
       }).then((result) => {
         setData(result)
       })
-  }, [])
+  }
 
   return (
     <>
@@ -22,11 +26,11 @@ function Blog() {
           <div className='container'>
             <div className='blog-main'>
               {
-                data.map((item) => {
+                data.map((item, index) => {
                   let d = new Date(item.date);
                   item.date = d.toString().split(" ")[1] + " " + d.toString().split(" ")[2] + "," + d.toString().split(" ")[3]
                   return <div className='blog-box'>
-                    <div className='blog-box-img'>
+                    <div className='blog-box-img' onClick={() => { setFlag(true); setIndex(index) }}>
                       <img src={item.thumbnail.large} alt="Blog_Image" />
                       <span className='img-hover'>Learn More</span>
                     </div>
@@ -44,12 +48,38 @@ function Blog() {
                     </div>
                   </div>
                 })
-
               }
             </div>
+            {flag ? ( <div className='model-wpr'>
+              <div className='model-overlay'>
+               <div className='model'>
+              <span className='close-icon' onClick={() => { setFlag(false) }}>X</span>
+              {
+                data.map((item, index) => {
+                  return Index === index ? (<div>
+                    <div className='model-image'>
+                      <img src={item.thumbnail.large} alt="Blog_Image" />
+                    </div>
+                    <div className='model-detail'>
+                      <h2 className='model-title'>{item.title}</h2>
+                      <p className='text-detail'>{item.content}</p>
+                      <div className='model-bottom'>
+                      <span className='avatar'>
+                        <img src={item.author.avatar} alt='author'/>
+                      </span>
+                      <span className='author-name'>{item.author.name} - {item.author.role}</span>
+                    </div>
+                    </div>
+                 
+                  </div>) : null
+                })}
+            </div> 
+            </div>
+            </div>) : null}
           </div>
         </div>
       </div>
+
     </>
   );
 }
